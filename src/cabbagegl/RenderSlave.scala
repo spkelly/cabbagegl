a27d093c439127aff976885f174b39d290b0424b
ChartrandEtienne <chartrand.etienne@gmail.com>
2013-04-30 22:43:29 -0400

    Got the termination working
    Kind of weird, again not my best work, but that'l work better with
    multiple actors.
    
    Which I think I need to be reminded that it was the *goal*

--- src/cabbagegl/RenderSlave.scala
+++ src/cabbagegl/RenderSlave.scala
@@ -5,25 +5,38 @@ import scala.actors.OutputChannel
 import java.awt.image.BufferedImage
 import java.awt.Color
 
-case class AskPixel(i: Int, j: Int) 
+class Message
 
-class BuildAskPixel(var iter: IndexedSeq[(Int, Int)]) {
-  // def this(a: Int, b: Int) = this(for (i <- 0 to (a - 1); j <- 0 to (b - 1)) yield (i, j))
-  def next = if (iter.isEmpty) None
-    else {
-    val buffer = Some(AskPixel(iter.head._1, iter.head._2))
-    iter = iter.tail
-    buffer 
-    }
-}
+case class AskPixel(i: Int, j: Int) extends Message
 
-case class GetPixel(i: Int, j: Int, color: Int) {
+case class GetPixel(i: Int, j: Int, color: Int) extends Message { 
+
   override def toString = i + ", " + j + ", " + color
 }
 
-case class ImgWrapper(img: BufferedImage)
+case class ImgWrapper(img: BufferedImage) extends Message
 
-case class End
+case class SemiEnd extends Message
+
+case class TotalEnd extends Message
+
+class BuildAskPixel(var iter: IndexedSeq[(Int, Int)]) extends Message {
+
+  // def this(a: Int, b: Int) = this(for (i <- 0 to (a - 1); j <- 0 to (b - 1)) yield (i, j))
+  var njobs = 0
+  def next = if (iter.isEmpty) {
+      if (njobs == 0) TotalEnd else SemiEnd
+    } 
+    else {
+    val buffer = AskPixel(iter.head._1, iter.head._2)
+    njobs += 1
+    iter = iter.tail
+    buffer 
+    }
+  def gotPixel = njobs -= 1
+}
+
+
 
 
 class RenderSlave(cam: Camera) extends Actor {
@@ -34,7 +47,7 @@ class RenderSlave(cam: Camera) extends Actor {
           val pixel = GetPixel(i, j, cam.renderPixel(i, j).getRGB())
           sender ! pixel
           // println("wot: " + pixel)
-        case End => exit()
+        case SemiEnd => exit()
         case _ => println("wtf")
       }
     }
@@ -54,18 +67,27 @@ class RenderMaster(cam: Camera, slave: RenderSlave) extends Actor {
           returnTo = Some(sender)
           img = image
           askFactory = new BuildAskPixel(for (i <- 0 to (cam.roptions.width - 1); j <- 0 to (cam.roptions.height - 1)) yield (i, j))
-          slave ! askFactory.next.get // just can't be a none // I should do something anyways
+          slave ! askFactory.next // just can't be like anything but an askPixel. Except you have more actors than pixels. Right? 
         }
         case GetPixel(a, b, color) => {
           println("master: got GetPixel; i: " + a + " , j: " + b)
           img.setRGB(a, b, color)
+          askFactory.gotPixel
           askFactory.next match {
-            case Some(toAsk) => sender ! toAsk
-            case None => {  
+            case toAsk: AskPixel => sender ! toAsk
+            case TotalEnd => {  
               loopPixels = false
-              sender ! End
+              sender ! SemiEnd
               returnTo.get ! ImgWrapper(img)
+              println("master: totalEnd")
+              exit()
             }
+            case SemiEnd => {
+              askFactory.gotPixel
+              sender ! SemiEnd
+              println("master: semiEnd")
+            }
+            case _ => println("master: wtf")
           }
         }
       }
